f0d3d8c20d581a9c47736d6deae5a170e2afac84
collin stasiak <collinstasiak@gmail.com>
2014-03-17 12:12:06 -0400

    made so the blocks fall down

--- graphictest.py
+++ graphictest.py
@@ -9,6 +9,8 @@
  Explanation video: http://youtu.be/vRB_983kUMc
 """
 import random
+import time
+
 import pygame
 
 # Define some colors
@@ -20,13 +22,6 @@ RED      = ( 255,   0,   0)
 pygame.init()
 
 # Set the width and height of the screen [width, height]
-size = (300, 300)
-screen = pygame.display.set_mode(size)
-
-pygame.display.set_caption("My Game")
-
-#Loop until the user clicks the close button.
-done = False
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
@@ -36,8 +31,6 @@ tiles = {'r0': (0, 0),
 	'r3': (0, 200),
 	'r4': (200, 200)
 }
-random_tile = random.choice(tiles.keys())
-
 
 def is_cursor_in_rectangle(size, rectangle_position, cursor_position):
 	if rectangle_position[0] + size <= cursor_position[0]:
@@ -51,22 +44,32 @@ def is_cursor_in_rectangle(size, rectangle_position, cursor_position):
 	return True
 
 # -------- Main Program Loop -----------
-center = {'x':size[0]/2, 'y':size[1]/2}
-rectangle_size = 100
-screen.fill(WHITE)
-position = (0, 0)
-for key, value in tiles.items():
-	pygame.draw.rect(screen, BLACK, [ value[0], value[1], rectangle_size, rectangle_size])
-while not done:
-	# --- Main event loop
-	for event in pygame.event.get(): # User did something
-		if event.type == pygame.QUIT: # If user clicked close
-			done = True # Flag that we are done so we exit this loop
-		elif event.type == pygame.MOUSEBUTTONDOWN:
-			position = pygame.mouse.get_pos()
+def game(level):
+	random_tile = random.choice(tiles.keys())
+	size = (300, 300)
+	screen = pygame.display.set_mode(size)
+	center = {'x':size[0]/2, 'y':size[1]/2}
+	rectangle_size = 100
+	screen.fill(WHITE)
+	position = (0, 0)
+	pygame.display.set_caption("Level " + str(level))
+	tries = 6 - level
+	for key, value in tiles.items():
+		pygame.draw.rect(screen, BLACK, [ value[0], value[1], rectangle_size, rectangle_size])
+	while tries:
+		# --- Main event loop
+		decrement_tries = False
+		for event in pygame.event.get(): # User did something
+			if event.type == pygame.QUIT: # If user clicked close
+				return False
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				position = pygame.mouse.get_pos()
+				decrement_tries = True
 
 		for key, value in tiles.items():
 			if is_cursor_in_rectangle(rectangle_size, value, position):
+				if decrement_tries:
+					tries -= 1
 				if random_tile == key:
 					color = GREEN
 				else:
@@ -74,9 +77,18 @@ while not done:
 			else:
 				color = BLACK
 			pygame.draw.rect(screen, color, [value[0], value[1], rectangle_size, rectangle_size])
-	# --- Game logic should go here
-	pygame.display.flip()
+			if color == GREEN:
+				pygame.display.flip()
+				time.sleep(1)
+				return True
+		# --- Game logic should go here
+		pygame.display.flip()
 
-	# --- Limit to 60 frames per second
-	clock.tick(60)
+		# --- Limit to 60 frames per second
+		clock.tick(60)
+	return False
+
+for level in range(1, 6):
+	if not game(level):
+		break
 pygame.quit()
