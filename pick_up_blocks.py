0391bd8b559630878f9b0f84d0a55738713c6be3
collin stasiak <collinstasiak@gmail.com>
2014-04-15 17:42:18 -0400

    put htings into a main method

--- pick_up_blocks.py
+++ pick_up_blocks.py
@@ -1,19 +1,8 @@
-"""
-Use sprites to collect blocks.
-
-Sample Python/Pygame Programs
-Simpson College Computer Science
-http://programarcadegames.com/
-http://simpson.edu/computer-science/
-
-Explanation video: http://youtu.be/4W2AqUetBi4
-"""
-#1. break out the main loop into function
-#2. draw the blocks to random part of screen but the block moves down slowly
-#
-import pygame
-import random
 import os
+import random
+import time
+
+import pygame
 
 # Define some colors
 BLACK = (  0,   0,   0)
@@ -27,12 +16,6 @@ BULLET_WIDTH = 15
 BULLET_HEIGHT = 30
 
 class Block(pygame.sprite.Sprite):
-	"""
-	This class represents the ball.
-	It derives from the "Sprite" class in Pygame.
-	"""
-	# Constructor. Pass in the color of the block,
-	# and its x and y position
 	def __init__(self, image_file):
 		# Call the parent class (Sprite) constructor
 		pygame.sprite.Sprite.__init__(self)
@@ -72,81 +55,80 @@ class Bullet(pygame.sprite.Sprite):
 	def update(self):
 		self.rect.y -= 2
 
-pygame.init()
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-block_list = pygame.sprite.Group()
-bullet_list = pygame.sprite.Group()
-all_sprites_list = pygame.sprite.Group()
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+	block_list = pygame.sprite.Group()
+	bullet_list = pygame.sprite.Group()
+	all_sprites_list = pygame.sprite.Group()
+	for i in range(20):
+		# This represents a block
+		block = Block('enemy_1.bmp')
+		# Set a random location for the block
+		block.rect.x = random.randrange(SCREEN_WIDTH)
+		block.rect.y = random.randrange(SCREEN_HEIGHT)
+		# Add the block to the list of objects
+		block_list.add(block)
+		all_sprites_list.add(block)
+	player = Player()
+	player.center_player()
+	lives_left = 3
+	all_sprites_list.add(player)
+	done = False
+	clock = pygame.time.Clock()
+	blocks_hit_list = pygame.sprite.spritecollide(player, block_list, False)
+	map(lambda block: block.reset_pos(), blocks_hit_list)
+	score = 0
+	last_shot = 0
+	while not done:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					done = True
+			keys_pressed = pygame.key.get_pressed()
+			if keys_pressed[pygame.K_LEFT]:
+				if player.rect.x > 0:
+					player.rect.x -= MOVEMENT
+			if keys_pressed[pygame.K_RIGHT]:
+				if player.rect.x <= SCREEN_WIDTH - player.image.get_width():
+					player.rect.x += MOVEMENT
+			if keys_pressed[pygame.K_SPACE]:
+				if ((time.time() - last_shot) > 0.5) and (len(bullet_list) < 5):
+					last_shot = time.time()
+					position = [player.rect.x, player.rect.y]
+					position[0]  = position[0] + player.image.get_width() / 2
+					position[0] = position[0] - (BULLET_WIDTH / 2)
+					bullet = Bullet(position)
+					bullet_list.add(bullet)
+					all_sprites_list.add(bullet)
+			screen.fill(WHITE)
+			blocks_hit_list = pygame.sprite.spritecollide(player, block_list, False)
+			map(lambda block: block.reset_pos(), blocks_hit_list)
+			if len(blocks_hit_list):
+				lives_left -= 1
+				player.center_player()
+				if lives_left == 0:
+					print('You lost')
+					done = True
+			for bullet in bullet_list:
+				blocks_hit_list = pygame.sprite.spritecollide(bullet, block_list, False)
+				for block in blocks_hit_list:
+					block.reset_pos()
+				score += len(blocks_hit_list)
+				if bullet.rect.y < -BULLET_HEIGHT:
+					bullet_list.remove(bullet)
+					all_sprites_list.remove(bullet)
+			all_sprites_list.draw(screen)
 
-for i in range(20):
-	# This represents a block
-	block = Block('enemy_1.bmp')
+			block_list.update()
+			bullet_list.update()
 
-	# Set a random location for the block
-	block.rect.x = random.randrange(SCREEN_WIDTH)
-	block.rect.y = random.randrange(SCREEN_HEIGHT)
+			# Limit to 60 frames per second
+			clock.tick(60)
 
-	# Add the block to the list of objects
-	block_list.add(block)
-	all_sprites_list.add(block)
+			# Go ahead and update the screen with what we've drawn.
+			pygame.display.flip()
+	print('Your score was: ' + str(score))
+	pygame.quit()
 
-# Create a RED player block
-player = Player()
-player.center_player()
-lives_left = 3
-
-all_sprites_list.add(player)
-
-#Loop until the user clicks the close button.
-done = False
-
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-
-# See if the player block has collided with anything.
-blocks_hit_list = pygame.sprite.spritecollide(player, block_list, False)
-for block in blocks_hit_list:
-	block.reset_pos()
-
-score = 0
-
-# -------- Main Program Loop -----------
-while not done:
-	for event in pygame.event.get(): # User did something
-		if event.type == pygame.QUIT: # If user clicked close
-			done = True
-
-	keys_pressed = pygame.key.get_pressed()
-	if keys_pressed[pygame.K_LEFT]:
-		if player.rect.x > 0:
-			player.rect.x -= MOVEMENT
-	if keys_pressed[pygame.K_RIGHT]:
-		if player.rect.x <= SCREEN_WIDTH - player.image.get_width():
-			player.rect.x += MOVEMENT
-	if keys_pressed[pygame.K_SPACE]:
-		position = [player.rect.x, player.rect.y]
-		position[0] = position[0] + player.image.get_width() / 2
-		position[0] = position[0] - (BULLET_WIDTH / 2)
-		bullet = Bullet(position)
-		bullet_list.add(bullet)
-		all_sprites_list.add(bullet)
-	screen.fill(WHITE)
-	blocks_hit_list = pygame.sprite.spritecollide(player, block_list, True)
-	if len(blocks_hit_list):
-		lives_left -= 1
-		player.center_player()
-		if lives_left == 0:
-			print('You lost')
-			done = True
-	all_sprites_list.draw(screen)
-
-	block_list.update()
-	bullet_list.update()
-
-	# Limit to 60 frames per second
-	clock.tick(60)
-
-	# Go ahead and update the screen with what we've drawn.
-	pygame.display.flip()
-
-pygame.quit()
+if __name__ == '__main__':
+	main()
